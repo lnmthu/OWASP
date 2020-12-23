190c3e4fc40c3c1be5a021b93e5b88097c3d59c8
minhthu <minhthu512586@gmail.com>
2020-12-23 20:39:39 +0700

    delete all master v1

--- storage/framework/views/db9ad4b4a24f64441ea403490ad2544a12353eb7.php
+++
@@ -1,8 +0,0 @@
-<div class="barLeft">
-    <div class="icon"><a>Biểu tượng Web</a></div>
-    <div class="menu"><a>Trang chủ</a></div>
-    <div class="menu"><a>Giới thiệu</a></div>
-    <div class="menu"><a>Mức bảo mật</a></div>
-    <div class="menu"><a>Top 10 OWASP</a></div>
-    <div class="menu" style="margin-bottom: 400px;"><a>About</a></div>
-  </div><?php /**PATH /var/www/html/resources/views/show/layout/barleft.blade.php ENDPATH**/ ?>
