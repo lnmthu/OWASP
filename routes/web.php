53b1c26c8fdad42d458a6ad543b76e7e3af7b456
minhthu <minhthu512586@gmail.com>
2020-12-23 21:05:29 +0700

    update branch

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// Route::get("/", "XssController@showReflectedXss");
 //show
 Route::get("reflected-xss", "XssController@showReflectedXss");
-// Route::get("sql-injection", "SqlController@home");
 
 //action
 Route::prefix('action')->group(function () {
@@ -24,19 +22,5 @@ Route::prefix('action')->group(function () {
         Route::get('show-code-prevent','XssController@showCodePrevent');
         Route::get('get-code-prevent','XssController@getCodePrevent');
     });
-    // Route::prefix('sql')->group(function () {
-    //     Route::get("reset","SqlController@reset");
-    //     Route::get('login', 'SqlController@getLogin');
-    //     Route::post('login', 'SqlController@postLogin');
-    //     Route::get('code', 'SqlController@actionShowCode');
-    //     Route::get('home', 'SqlController@actionHome')->middleware('check.without.auth');
-    //     Route::post('search', 'SqlController@postSearch');
-    //     Route::get('logout','SqlController@logout');
-    //     Route::get('show-code-prevent','SqlController@showCodePrevent');
-    //     Route::post('post-code-prevent','SqlController@postCodePrevent');
 
-    // });
-    // Route::prefix('broken-auth')->group(function () {
-    //     Route::get("terminal","BrokenAuthController@terminal");
-    // });
 });
